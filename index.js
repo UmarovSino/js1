5a5952be0a0a41b2004f4ef65eecf93241e43288
UmarovSino <abubakrs058@gmail.com>
2025-01-07 01:43:07 +0500

    add file js

---
+++ index.js
@@ -0,0 +1,25 @@
+////The sum of 5 and 10 is 15
+// let num1=5;
+// let num2=10;
+// sum=num1+num2;
+// console.log(sum);
+
+/// зарби рақами 3-ро аз 1 то 5 чоп кунед.
+// let num1=3;
+// for(let i=1;i<=5; i++)
+// {
+//     console.log(num1*i);
+// }
+////Ҷумлаи "Learning C++ is fun!"-ро панҷ маротиба, ҳар кадом дар сатр нав, чоп кунед.
+// let string="Learning C++ is fun!";
+// for (let i=1;i<=5;i++)
+// {
+//     console.log("Learning C++ is fun!");
+// }
+////Рақамҳои 50 ва 10-ро дар поёни якдигар нишон диҳед.
+// let nem=50;
+// let nem1=10;
+// console.log(nem);
+// console.log(nem1);
+
+let 
